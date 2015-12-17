64e6b4fb54064bfafac115cf4e4497524f793684
yarbroughw <wyarbrough93@gmail.com>
2015-12-16 22:07:40 -0700

    Completed day3 solution module

---
+++ src/Day3/Solution.hs
@@ -0,0 +1,53 @@
+module Day3.Solution where
+
+import Data.Set (fromList, union)
+
+type House = (Int, Int)
+data Direction = North | East | South | West
+
+
+readDirection :: Char -> Direction
+readDirection '^' = North
+readDirection '>' = East
+readDirection 'v' = South
+readDirection '<' = West
+
+
+travelFrom :: House -> Direction -> House
+travelFrom (x,y) d = case d of
+  North -> (x,y+1)
+  East  -> (x+1,y)
+  South -> (x,y-1)
+  West  -> (x-1,y)
+
+
+travel :: [Direction] -> [House]
+travel directions = scanl travelFrom (0,0) directions
+
+
+travelBy :: (Int -> Bool) -> [Direction] -> [House]
+travelBy pred directions = travel $ filtered directions
+  where filtered = map snd . filter (pred . fst) . zip [1..]
+
+
+numDistinct :: (Ord a) => [a] -> Int
+numDistinct = length . fromList
+
+
+part1 :: [Direction] -> Int
+part1 directions = numDistinct houses
+    where houses = travel directions
+
+
+part2 :: [Direction] -> Int
+part2 directions = numDistinct houses
+    where houses = santaHouses ++ robotHouses
+          santaHouses = travelBy even directions
+          robotHouses = travelBy odd directions
+
+
+day3 :: IO ()
+day3 = do
+  directions <- (map readDirection) <$> readFile "input.txt"
+  print $ part1 directions
+  print $ part2 directions
