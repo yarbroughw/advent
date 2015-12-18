d2490c5b07975c3bf11c81f454d43d1c2ef2ca4a
yarbroughw <wyarbrough93@gmail.com>
2015-12-18 12:38:51 -0700

    Added day1 solution

--- src/Day1/Solution.hs
+++ src/Day1/Solution.hs
@@ -1,3 +1,29 @@
 module Day1.Solution where
 
-day1 = "foo"
+import Data.List (findIndex, inits)
+
+-- slower than a foldr (does 2 passes over input), but more readable/declarative
+floorCount :: String -> Int
+floorCount input = ups - downs
+  where ups   = charCount '(' input
+        downs = charCount ')' input
+        charCount c = length . filter (== c)
+
+
+floorCount2 :: String -> Int
+floorCount2 = foldr ((+) . parenvals) 0
+  where parenvals '(' = 1
+        parenvals ')' = (-1)
+        parenvals _ = 0
+
+
+hitBasement :: String -> Maybe Int
+hitBasement input = findIndex (< 0) trip
+  where trip = map floorCount $ inits input
+
+
+day1 :: IO ()
+day1 = do
+  input <- readFile "input.txt"
+  print $ floorCount input  -- part 1
+  print $ hitBasement input -- part 2
