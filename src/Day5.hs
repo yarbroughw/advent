c09546580289fcebd7d41fb48aa6b66985ca04b7
yarbroughw <wyarbrough93@gmail.com>
2015-12-24 11:02:59 -0800

    Completed day5 solution

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,13 +1,36 @@
+module Day5 where
+
+import Data.List (group, isInfixOf, tails)
 
 threeVowels :: String -> Bool
-threeVowels = null . filter (`elem` "aeiouAEIOU")
+threeVowels = (>= 3) . length . filter (`elem` "aeiouAEIOU")
 
 doubleLetter :: String -> Bool
-doubleLetter = undefined
+doubleLetter = not . null . filter ((>1) . length) . group
 
 noBadStrings :: String -> Bool
-noBadStrings = undefined
+noBadStrings input = and $ map (not . flip isInfixOf input) badstrings
+  where badstrings = ["ab", "cd", "pq", "xy"]
+
+hasPalindromeTriplet :: String -> Bool
+hasPalindromeTriplet = or . map isPalindrome . triplets
+  where isPalindrome = (==) <*> reverse
+        triplets input = take ((length input)-3+1) $ map (take 3) $ tails input
+
+repeatedPair :: String -> Bool
+repeatedPair = or . map repeatedPrefixPair . takeWhile ((>=4) . length) . tails
+  where repeatedPrefixPair input = (take 2 input) `isInfixOf` (drop 2 input)
+
+applyRules :: [String -> Bool] -> String -> Bool
+applyRules rules string = and $ map ($ string) rules
 
 isNice :: String -> Bool
-isNice string = and $ map ($ string) rules
-  where rules = [threeVowels, doubleLetter, noBadStrings]
+isNice = applyRules [threeVowels, doubleLetter, noBadStrings]
+
+isNicer :: String -> Bool
+isNicer = applyRules [hasPalindromeTriplet, repeatedPair]
+
+solution :: String -> IO ()
+solution input = do
+  print $ length $ filter isNice (lines input)
+  print $ length $ filter isNicer (lines input)
